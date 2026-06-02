03117905af454e8fa748e02277bd1b65dcf7fc56
Nishchay Agarwal <116452777+yforyash@users.noreply.github.com>
2026-06-02 11:35:54 +0530

    Update 004_create_locations.sql

---
+++ Q3/backend/migrations/004_create_locations.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS locations (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  latitude DECIMAL(10, 7) NOT NULL,
+  longitude DECIMAL(10, 7) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
